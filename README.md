af19ba75c00464025ceb1c43fae2b11f381c346c
booyaa <booyaabooyaabooyaa@gmail.com>
2016-08-26 09:58:58 +0100

    :shipit:

---
+++ README.md
@@ -0,0 +1,11 @@
+# quack
+
+A [Duck Duck Go](https://duckduckgo.com) client.
+
+Not yet usable come back later.
+
+Copyright 2016 Mark Sta Ana.
+
+Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+http://www.apache.org/licenses/LICENSE-2.0> at your option. This file may not
+be copied, modified, or distributed except according to those terms.
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(feature = "serde_macros", feature(plugin, custom_derive))]
+#![cfg_attr(feature = "serde_macros", plugin(serde_macros))]
+
+extern crate serde;
+extern crate serde_json;
+
+#[cfg(feature = "serde_macros")]
+include!("serde_types.in.rs");
+
+#[cfg(feature = "serde_codegen")]
+include!(concat!(env!("OUT_DIR"), "/serde_types.rs"));
+
+fn main() {
+    let point = Point { x: 1, y: 2 };
+
+    let serialized = serde_json::to_string(&point).unwrap();
+    println!("serialized = {}", serialized);
+
+    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+    println!("deserialized = {:?}", deserialized);
+}
