453e46bd2dce0cc57c7ef546975aa659bb77d3d8
booyaa <booyaabooyaabooyaa@gmail.com>
2016-08-27 11:23:14 +0100

    InstantAnswer struct

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 extern crate serde;
 extern crate serde_json;
+extern crate hyper;
 
 #[cfg(feature = "serde_macros")]
 include!("serde_types.in.rs");
@@ -10,12 +11,55 @@ include!("serde_types.in.rs");
 #[cfg(feature = "serde_codegen")]
 include!(concat!(env!("OUT_DIR"), "/serde_types.rs"));
 
+use hyper::Client;
+use std::io::Read;
+
 fn main() {
-    let point = Point { x: 1, y: 2 };
+    let client = Client::new();
+    // ok - bang
+    // let mut res = client.get("http://api.duckduckgo.\
+    //                           com/?q=!imdb+rushmore&format=json&pretty=1&no_redirect=1")
+    //                     .send()
+    //                     .unwrap();
 
-    let serialized = serde_json::to_string(&point).unwrap();
-    println!("serialized = {}", serialized);
+    // ok - topic
+    // let mut res = client.get("http://api.duckduckgo.\
+    //                           com/?q=valley+forge+national+park&format=json&pretty=1")
+    //                     .send()
+    //                     .unwrap();
 
-    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
-    println!("deserialized = {:?}", deserialized);
+    // panics - category
+    let mut res = client.get("http://api.duckduckgo.\
+                              com/?q=simpsons+characters&format=json&pretty=1")
+                        .send()
+                        .unwrap();
+
+    // panics - disamibiguation
+    // let mut res = client.get("http://api.duckduckgo.com/?q=apple&format=json&pretty=1")
+    //                     .send()
+    //                     .unwrap();
+
+    let mut buffer = String::new();
+    res.read_to_string(&mut buffer).expect("no body");
+    println!("{}", buffer);
+
+    let instant_answer: InstantAnswer = serde_json::from_str(&buffer).unwrap();
+    println!("{:#?}", instant_answer);
 }
+
+// test cases
+// topic summaries
+// http://api.duckduckgo.com/?q=valley+forge+national+park&format=json&pretty=1
+// HTTP/1.1 200 OK
+// Server: nginx
+// Date: Sat, 27 Aug 2016 10:18:38 GMT
+// Content-Type: application/x-javascript
+// Connection: keep-alive
+// Access-Control-Allow-Origin: *
+// X-DuckDuckGo-Results: 1
+// Expires: Sat, 27 Aug 2016 10:18:39 GMT
+// Cache-Control: max-age=1
+// X-DuckDuckGo-Locale: en_US
+// categories - http://api.duckduckgo.com/?q=simpsons+characters&format=json&pretty=1
+// disamibiguation - http://api.duckduckgo.com/?q=apple&format=json&pretty=1
+// !bang redirect n.b. no_redirect otherwise you'll get a 303 - http://api.duckduckgo.com/?q=!imdb+rushmore&format=json&pretty=1&no_redirect=1
--- src/serde_types.in.rs
+++ src/serde_types.in.rs
@@ -1,5 +1,58 @@
+// strategies for handling null RelatedTopics (doesn't appear to be a problem in v0.8 serde)
+// http://stackoverflow.com/questions/37870428/convert-two-types-into-a-single-type-with-serde
+// http://stackoverflow.com/questions/38037235/handling-mixed-object-arrays-in-serde
 #[derive(Serialize, Deserialize, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
+struct InstantAnswer {
+    #[serde(rename = "Abstract")]
+    abstract_rich: String,
+    #[serde(rename = "AbstractText")]
+    abstract_text: String,
+    #[serde(rename = "AbstractSource")]
+    abstract_source: String,
+    #[serde(rename = "AbstractURL")]
+    abstract_url: String,
+    #[serde(rename = "Image")]
+    image: String,
+    #[serde(rename = "Heading")]
+    heading: String,
+    #[serde(rename = "Answer")]
+    answer: String,
+    #[serde(rename = "Redirect")]
+    redirect: String,
+    #[serde(rename = "AnswerType")]
+    answer_type: String,
+    #[serde(rename = "Definition")]
+    definition: String,
+    #[serde(rename = "DefinitionSource")]
+    definition_source: String,
+    #[serde(rename = "DefinitionURL")]
+    definition_url: String,
+    #[serde(rename = "RelatedTopics")]
+    related_topics: Vec<RelatedTopic>,
+    #[serde(rename = "Results")]
+    results: Vec<RelatedTopic>,
+    #[serde(rename = "Type")]
+    page_type: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct RelatedTopic {
+    #[serde(rename = "Result")]
+    result: String,
+    #[serde(rename = "Icon")]
+    icon: Icon,
+    #[serde(rename = "FirstURL")]
+    first_url: String,
+    #[serde(rename = "Text")]
+    text: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Icon {
+    #[serde(rename = "URL")]
+    url: String,
+    #[serde(rename = "Height")]
+    height: usize,
+    #[serde(rename = "Width")]
+    width: usize,
 }
